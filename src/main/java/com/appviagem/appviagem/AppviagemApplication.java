0f317d704cf74dd8387124cb5fa5591bfeaf704f
italo <italo.almeida@outlook.com.br>
2022-10-05 19:23:29 -0300

    3 Entrega Individual

---
+++ src/main/java/com/appviagem/appviagem/AppviagemApplication.java
@@ -0,0 +1,13 @@
+package com.appviagem.appviagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppviagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppviagemApplication.class, args);
+	}
+
+}
